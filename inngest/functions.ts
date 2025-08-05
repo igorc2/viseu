ad253e315a405ac378d2571c9fbfd9032490121c
Igor Cunha <igorc2@gmail.com>
2025-08-05 09:50:51 +0100

    Add @e2b/code-interpreter dependency and implement sandbox functionality. Create utility for sandbox management and update helloWorld function to return sandbox URL. Add Dockerfile and shell script for Next.js sandbox template.

--- inngest/functions.ts
+++ inngest/functions.ts
@@ -1,11 +1,17 @@
-
+import { Sandbox } from "@e2b/code-interpreter";
 import { openai, createAgent } from "@inngest/agent-kit";
 import { inngest } from "./client";
+import { getSandbox } from "./utils";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event }) => {
+  async ({ event, step }) => {
+
+    const sandboxId = await step.run("get-sandbox-id", async () => {
+      const sandbox = await Sandbox.create("vibe-nextjs-c2igor");
+      return sandbox.sandboxId;
+    });
 
     const codeAgent = createAgent({
       name: "symmarizer",
@@ -16,9 +22,15 @@ export const helloWorld = inngest.createFunction(
     const { output } = await codeAgent.run(
       `Write the following snippet ${event.data.value}`
     )
+    
+    const sandboxUrl = await step.run("get-sandbox-url", async () => {
+      const sandbox = await getSandbox(sandboxId);
+      const host = await sandbox.getHost(3000);
+      return `https://${host}`;
+    });
 
     console.log(output);
 
-    return { output };
+    return { output, sandboxUrl };
   },
 );
